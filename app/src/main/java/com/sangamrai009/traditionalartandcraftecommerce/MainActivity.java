c022c98acbcef122c3d417a727e8566077602150
Sangam Rai <adambantawa@gmail.com>
2019-11-21 12:27:44 +0545

    Initial commit

---
+++ app/src/main/java/com/sangamrai009/traditionalartandcraftecommerce/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sangamrai009.traditionalartandcraftecommerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
